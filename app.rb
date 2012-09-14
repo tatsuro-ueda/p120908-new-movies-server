64eab63a86164d26d7a0477509dd0308df740a0d
weed <weed_7777@yahoo.co.jp>
2012-09-14 18:52:00 +0900

    Vimeoあきらめる

--- app.rb
+++ app.rb
@@ -47,13 +47,13 @@ get '/new_movie' do
   feed_hatena1 = feed_hatena(params['tag1'])
   if params['tag2']
     feed_hatena2 = feed_hatena(params['tag2'])
-    feed_vimeo = feed_vimeo(params['tag2'])
+    # feed_vimeo = feed_vimeo(params['tag2'])
   end
   feed_nico = feed_nico(params['tag1'])
   if params['tag2']
     feed = feed_hatena1.append(
-      feed_hatena2, feed_nico, feed_vimeo).
-      # feed_hatena2, feed_nico).
+      # feed_hatena2, feed_nico, feed_vimeo).
+      feed_hatena2, feed_nico).
       unique
   else
     feed = feed_hatena1.append(feed_nico).unique
